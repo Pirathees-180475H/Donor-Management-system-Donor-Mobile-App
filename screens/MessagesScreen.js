36a3f5bb605fe021c20b1a6368fd4271194c57e7
Pirathees <kamalanathanpirathees@gmail.com>
2022-03-07 04:29:08 +0530

    minor Change

--- screens/MessagesScreen.js
+++ screens/MessagesScreen.js
@@ -4,7 +4,7 @@ export default MessagesScreen=({navigation})=> {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Donor Home Screen Contain Notifications</Text>
-         <Button title='go to Donation History' onPress={()=>navigation.navigate("DonationHistory")}></Button>
+         <Button title='go to Donation History ' onPress={()=>navigation.navigate("DonationHistory")}></Button>
       </View>
     );
   }
